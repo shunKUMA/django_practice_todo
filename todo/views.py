7289e44e62e676fcff32df132e93ead6fa79cad5
shunKUMA <kumachan4874@gmail.com>
2021-08-10 16:02:21 +0900

    listviewの追記

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import TodoModel
 
 # Create your views here.
+
+
+class Todolist(ListView):
+    template_name = 'list.html'
+    model = TodoModel
