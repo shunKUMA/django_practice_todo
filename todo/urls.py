4aca06c428208d8f3d584beda75ac6751dc165a4
shunKUMA <kumachan4874@gmail.com>
2021-08-11 15:40:02 +0900

    delete.htmlの作成

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import Todolist, TodoDetail, TodoCreate
+from .views import Todolist, TodoDetail, TodoCreate, TodoDelete
 
 urlpatterns = [
     path('list/', Todolist.as_view(), name='list'),
     path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
     path('create/', TodoCreate.as_view(), name='create'),
+    path('delete/', TodoDelete.as_view(), name='delete'),
 ]
