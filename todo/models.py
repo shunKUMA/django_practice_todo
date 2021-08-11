ce635e5d5ff3c9deed5091122f78b2482de18049
shunKUMA <kumachan4874@gmail.com>
2021-08-11 15:08:32 +0900

    migrateを実行

--- todo/models.py
+++ todo/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+CHOICE = ('denger', 'high'), ('warning', 'normal'), ('primary', 'low')
+
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=CHOICE
+    )
+    duedate = models.DateTimeField()
 
     def __str__(self):
         return self.title
